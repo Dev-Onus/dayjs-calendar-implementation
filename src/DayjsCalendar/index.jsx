09ca00a977d9c6f6c8ec5b6be1a80523fa9fd046
Yogesh Surendran <carreraphp@gmail.com>
2020-11-07 02:16:38 +0530

    feat: enable jest

--- src/DayjsCalendar/index.jsx
+++ src/DayjsCalendar/index.jsx
@@ -1,105 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
+import CalendarHeader from "./header";
+import { getMonth, getYear } from "./utils";
 import dayjs from "dayjs";
-import "dayjs/locale/es";
-import * as ObjectSupport from "dayjs/plugin/objectSupport";
-import * as localeData from "dayjs/plugin/localeData";
-import * as weekOfYear from "dayjs/plugin/weekOfYear";
-import * as weekday from "dayjs/plugin/weekday";
-import * as weekYear from "dayjs/plugin/weekYear";
-import "./index.css";
-
-dayjs.extend(localeData);
-dayjs.extend(ObjectSupport);
-dayjs.extend(weekOfYear);
-dayjs.extend(weekYear);
-dayjs.extend(weekday);
-
-window.d1 = dayjs();
-window.dc = dayjs;
-
-const totalNoOfWeeks = 52;
-const maxNoOfWeeksInCalendar = 72;
-
-const getCalendarLocale = (displayLocale = "en") => {
-  dayjs.locale(displayLocale);
-  return [dayjs.monthsShort(), dayjs.weekdaysMin()];
-};
-
-const getFormattedDateString = (date) => {
-  return date.format("DD dd MM MMMM YYYY");
-};
-
-const fs = getFormattedDateString;
-
-const getDaysInMonths = (calendarRange, selectedMonth, selectedYear) => {
-  const lastOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const [jan, , ...rest] = lastOfMonths;
-  let yearofFeb = selectedYear;
-
-  switch (calendarRange) {
-    case "calendarYear": //starts in Mar, so feb will be next year
-      yearofFeb = selectedYear + 1;
-      break;
-    case "full": // need to decide based on the current month is.
-    case "year": // need to decide based on the current month is.
-    default:
-      yearofFeb = selectedMonth <= 1 ? selectedYear : selectedYear + 1;
-      break;
-  }
-
-  return [jan, dayjs().year(yearofFeb).month(1).daysInMonth(), ...rest];
-};
-
-const getFirstDayofMonths = () => new Array(12).fill(1);
-
-const getCalendarList = (
-  calendarRange = "year",
-  selectedDatetime = dayjs()
-) => {
-  // const selectedDate = selectedDatetime.date();
-  const selectedMonth = selectedDatetime.month();
-  const selectedYear = selectedDatetime.year();
-  const firstOfMonths = getFirstDayofMonths();
-  const lastOfMonths = getDaysInMonths(
-    calendarRange,
-    selectedMonth,
-    selectedYear
-  );
-  let calendarStart = null;
-  let calendarEnd = null;
-  let fluidLayout = false;
-
-  switch (calendarRange) {
-    case "calendarYear":
-      calendarStart = dayjs().month(3).date(1);
-      calendarEnd = dayjs()
-        .year(selectedYear + 1)
-        .month(2)
-        .date(lastOfMonths[2]);
-      break;
-    case "full":
-      calendarStart = dayjs().year(selectedYear).month(selectedMonth).date(1);
-      calendarEnd = calendarStart.clone().add({ M: 11 });
-      fluidLayout = true;
-      break;
-    case "year":
-    default:
-      calendarStart = dayjs().year(selectedYear).month(selectedMonth).date(1);
-      calendarEnd = calendarStart.clone().add({ M: 11 });
-      break;
-  }
-
-  return [
-    fs(calendarStart),
-    fs(calendarEnd),
-    fluidLayout,
-    firstOfMonths,
-    lastOfMonths
-  ];
-};
-
-const getScreenWidth = (ref, className) =>
-  ref.current.querySelector(`.${className}`).offsetWidth;
 
 const Calendar = ({
   calendarView = "normal", // normal or weekdays
@@ -107,142 +9,83 @@ const Calendar = ({
   displayLocale = "en", // en or es
   forwardedRef = null
 }) => {
-  const uniq = Math.floor(Math.random() * 1000);
-  const [monthLocale, setMonthLocale] = useState(null);
-  const [weekLocale, setWeekLocale] = useState(null);
-  const [calendar, setCalendar] = useState([]);
-  const [daysInView, setDaysInView] = useState(
-    calendarView === "normal" ? 7 : 5
-  );
-  const [calenderLayout, setCalenderLayout] = useState([]);
-  const [monthClassNames, setMonthClassNames] = useState("month-normal");
-  const [containerClassNames, setContainerClassNames] = useState(
-    "container-normal"
-  );
-  const [isResized, setResized] = useState(false);
-  const [maxHeight, setMaxHeight] = useState(null);
+  const [enableWeekdays, setWeekDays] = useState(false);
+  const [layoutWidth, setLayoutWidth] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [monthPosition, setMonthPosition] = useState(0);
 
-  const cellsRef = useRef();
+  const calendarRef = useRef();
 
-  useEffect(() => {
-    let days = 7;
-    let monthClassName = "month-normal";
-    let containerClassName = "container-normal";
-    switch (calendarView) {
-      case "weekdays":
-        days = 5;
-        monthClassName = "month-weekdays";
-        containerClassName = "container-weekdays";
-        break;
-      case "normal":
-      default:
-        days = 7;
-        monthClassName = "month-normal";
-        containerClassName = "container-normal";
-        break;
-    }
-    setDaysInView(days);
-    setMonthClassNames(monthClassName);
-    setContainerClassNames(containerClassName);
-  }, [calendarView]);
-
-  // construct the array based on the days & weeks count.
-  useEffect(() => {
-    setCalenderLayout(
-      new Array(12)
-        .fill(" ")
-        .map((_, idx) => new Array(daysInView * 6).fill(" "))
-    );
-  }, [daysInView]);
-
-  //do something on mount
-  useEffect(() => {
-    const [months, weeks] = getCalendarLocale(displayLocale);
-    setMonthLocale(months);
-    setWeekLocale(weeks);
-  }, [displayLocale]);
-
-  //do something on mount
-  useEffect(() => {
-    setCalendar(getCalendarList(calendarRange));
-  }, [calendarRange, monthLocale, weekLocale]);
-
-  useEffect(() => {
-    // console.log(
-    //   containerRef.current,
-    //   containerRef.current.querySelector(".layout"),
-    //   cellsRef,
-    //   "cell conatinerRef"
-    // );
-    // const exactContainer = containerRef.current.querySelector(".layout");
-    // console.log(
-    //   exactContainer.querySelector("div.cell"),
-    //   containerRef.current.querySelector("div.cell"),
-    //   "cell"
-    // );
-    // console.log(
-    //   cellsRef.current,
-    //   cellsRef?.current?.clientWidth,
-    //   "overall width"
-    // );
-    // con
-    if (calenderLayout.length > 0 && isResized) {
-      setMaxHeight(
-        getScreenWidth(containerRef.current.querySelector(".cells")) *
-          daysInView
-      );
-    }
-  }, [isResized, calenderLayout]);
-
-  const handleResize = () => {
-    setResized(!isResized);
+  const getMonthHeight = () => {
+    const layoutElm = calendarRef?.current?.querySelector(".layout");
+    setLayoutWidth(layoutElm.getBoundingClientRect().width);
   };
 
-  console.log(calenderLayout, "calenderLayout");
+  useEffect(() => {
+    getMonthHeight();
+  }, []);
+
+  const getElemWidth = (ref, className) =>
+    ref.current.querySelector(`.${className}`).getBoundingClientRect().width;
+
+  const computeMaxHeight = (ref, className, numToMultiply) =>
+    getElemWidth(ref, className) * numToMultiply;
+
+  useEffect(() => {
+    const layoutElm = calendarRef?.current?.querySelector(".layout");
+    layoutElm.style.transform = "translate(0%, -" + scrollTop + "px)";
+  }, [scrollTop]);
+
+  const handleToggle = () => {
+    setWeekDays(!enableWeekdays);
+  };
+
+  const handleScrollUp = () => {
+    // const railElm = calendarRef?.current?.querySelector(".layout-rail");
+    // let monthHeight = (!enableWeekdays ? 0.86 : 1.2) * layoutWidth;
+    if (monthPosition < 10) {
+      setMonthPosition(monthPosition + 1);
+      setScrollTop(scrollTop + computeMaxHeight(calendarRef, "cell", 6));
+    }
+  };
+
+  const handleScrollDown = () => {
+    // const railElm = calendarRef?.current?.querySelector(".layout-rail");
+    // let monthHeight = (!enableWeekdays ? 0.86 : 1.2) * layoutWidth;
+    if (monthPosition > 0) {
+      setMonthPosition(monthPosition - 1);
+      setScrollTop(scrollTop - computeMaxHeight(calendarRef, "cell", 6));
+    }
+  };
 
   return (
     <div>
-      Hello, {calendarRange} - {displayLocale}
-      <br />
-      {JSON.stringify(monthLocale, null, 4)}
-      {JSON.stringify(weekLocale, null, 4)}
-      {/* {JSON.stringify(weekLocale, null, 4)} */}
-      {JSON.stringify(calendar, null, 4)}
-      <div style={{ width: "600px" }}>
-        <div className={`container ${containerClassNames}`} ref={forwardedRef}>
-          <div className="layout-rail">
-            <div
-              className={`layout `}
-              // onResize={handleResize}
-            >
-              {calenderLayout.map((monthLayout, index) => {
-                return (
-                  <div className={`month ${monthClassNames}`}>
-                    {monthLayout.map((value, index) => {
-                      return (
-                        <>
-                          <div className="cell" data-place-id={index}>
-                            <div className="aspect-ratio"></div>
-                            <div className="content-area">
-                              <div className="center-content">{index}</div>
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
+      <CalendarHeader
+        {...{
+          handleToggle,
+          handleScrollUp,
+          handleScrollDown,
+          monthPosition,
+          calendarRange: "calendarYear",
+          month: getMonth(dayjs()),
+          year: getYear(dayjs())
+        }}
+      />
+      <Calendar
+        forwardedRef={calendarRef}
+        displayLocale="en"
+        calendarRange="year"
+        calendarView={enableWeekdays ? "weekdays" : "normal"}
+      />
     </div>
   );
 };
 
 Calendar.defaultProps = {
-  calenderRange: "year", // year, calendarYear, full, custom
-  displayLocale: "en"
+  calendarView: "normal", // normal or weekdays
+  calendarRange: "calendarYear", // year, calendarYear, full, custom
+  displayLocale: "en", // en or es
+  forwardedRef: null
 };
+
 export default Calendar;
--- src/stories/dayjsCalendar.stories.js
+++ src/stories/dayjsCalendar.stories.js
@@ -1,70 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import Calendar from "../DayjsCalendar/index.jsx";
+import CalendarHeader from "../DayjsCalendar/header.jsx";
 // import dayjs from "dayjs";
+import { getFirstDayOffsetInMonths } from "../DayjsCalendar/utils";
+import results from "../../test-results.json";
+import { withTests } from "@storybook/addon-jest";
 
 export default {
   title: "DayjsCalendar",
-  component: Calendar
+  component: Calendar,
+  decorators: [withTests({ results })]
 };
 
-export const Year = () => {
-  const [enableWeekdays, setWeekDays] = useState(false);
-  const [layoutWidth, setLayoutWidth] = useState(0);
-  const [scrollTop, setScrollTop] = useState(0);
-
-  const calendarRef = useRef();
-
-  const getMonthHeight = () => {
-    const layoutElm = calendarRef?.current?.querySelector(".layout");
-    setLayoutWidth(layoutElm.getBoundingClientRect().width);
-  };
-
-  useEffect(() => {
-    getMonthHeight();
-  }, []);
-
-  useEffect(() => {
-    const layoutElm = calendarRef?.current?.querySelector(".layout");
-    // layoutElm.scrollTo(0, scrollTop);
-    console.log("Inn scrolTop####");
-    layoutElm.style.transform = "translate(0%, -" + scrollTop + "px)";
-  }, [scrollTop]);
-
-  const handleToggle = () => {
-    setWeekDays(!enableWeekdays);
-  };
-
-  const handleScrollUp = () => {
-    const railElm = calendarRef?.current?.querySelector(".layout-rail");
-    let monthHeight = (!enableWeekdays ? 0.86 : 1.2) * layoutWidth;
-    setScrollTop(scrollTop + monthHeight + 0);
-    // setScrollTop(scrollTop + 8.3);
-    // debugger;
-    // console.log(monthHeight, "monthHeight");
-  };
-
-  const handleScrollDown = () => {
-    const railElm = calendarRef?.current?.querySelector(".layout-rail");
-    let monthHeight = (!enableWeekdays ? 0.86 : 1.2) * layoutWidth;
-    setScrollTop(scrollTop - monthHeight + 0);
-    // setScrollTop(scrollTop - 8.3);
-    // debugger;
-  };
-
-  return (
-    <div>
-      <button onClick={handleToggle}>Toggle weekends</button>
-      <button onClick={handleScrollUp}>Scroll up</button>
-      <button onClick={handleScrollDown}>Scroll down</button>
-      <Calendar
-        forwardedRef={calendarRef}
-        displayLocale="en"
-        calendarRange="year"
-        calendarView={enableWeekdays ? "weekdays" : "normal"}
-      />
-    </div>
-  );
-};
+console.log(getFirstDayOffsetInMonths("", 10, 2020), "monthOffsets");
+export const Year = () => <Calendar displayLocale="en" calendarRange="year" />;
 
 export const CalendarYear = () => (
   <Calendar displayLocale="en" calendarRange="calendarYear" />
@@ -97,3 +46,8 @@ export const All = () => (
 //     </div>
 //   );
 // };
+
+export const defaultView = () => <div>Jest results in storybook</div>;
+defaultView.parameters = {
+  jest: ["LoginForm.test.js"]
+};
